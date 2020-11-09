875c1ebfac64802edbdd0f34a263e809a480b423
Jason Bohlinger <jbohlinger@chapman.edu>
2020-11-09 10:43:51 -0800

    Adds username.sh

---
+++ username.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# username.sh
+# Jason Bohlinger
+echo "Enter a username: "
+echo "Only lowercase letters, digits, and underscore."
+echo "The username must start with a lower case letter"
+echo "The username must contain at least 3 but no more than 12 characters"
+read USERNAME
+while echo "$USERNAME" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+do
+	echo "You must enter a valid username!"
+	echo "Enter a username: "
+	read USERNAME
+done
+echo "Thank you"
