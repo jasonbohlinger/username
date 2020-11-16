d9857b10717b57c8df3211a5553e69516238e4fe
Jason Bohlinger <jbohlinger@chapman.edu>
2020-11-16 10:38:54 -0800

    changed username.sh permissions

--- username.sh
+++ username.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # username.sh
 # Jason Bohlinger
-# Version 2.0
+# Version 3.0
 echo "Enter a username: "
 echo "Only lowercase letters, digits, and underscore."
 echo "The username must start with a lower case letter"
